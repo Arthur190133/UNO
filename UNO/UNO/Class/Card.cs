891a282cf9bb68442d902720a66581c846a63b7d
Arthur190133 <91669888+Arthur190133@users.noreply.github.com>
2022-04-27 20:53:05 +0200

    Ajouts des flèches de direction

---
+++ UNO/UNO/Class/Card.cs
@@ -0,0 +1,12 @@
+﻿using static UNO.Program;
+
+namespace UNO
+{
+    // classe de la carte
+    public class Card
+    {
+        public card_type types { get; set; }// type de la carte
+        public card_color color { get; set; }// couleur de la carte
+        public int number { get; set; }// numéro de la carte
+    }
+}
---
+++ UNO/UNO/Class/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using static UNO.Program;
+
+namespace UNO
+{
+    // classe du joueur
+    public class Player
+    {
+
+        public player_type player_type { get; set; }// type de joueur
+        public string player_name { get; set; }// nom du joueur
+        public List<Card> player_cards { get; set; }// liste des cartes du joueur
+        public int player_points { get; set; }// points du joueur
+
+        // initialiser les variables du joueur
+        public Player(player_type current_player_type, string current_player_name)
+        {
+            player_type = current_player_type;
+            player_name = current_player_name;
+            player_cards = new List<Card>();
+            player_points = int.MaxValue;
+        }
+    }
+}
